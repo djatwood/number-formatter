2bfbf8b290ac222f1b8c08b5564b191b0384683a
Daniel Atwood <daniel@atwood.io>
2018-10-11 18:17:53 -0400

    Refactoring everything

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ import('./format_number.js')
 .then(module => {
 		// Listen to all type="tel" inputs
 		document.querySelectorAll('input[type=tel][placeholder]').forEach(el => {
-			el.addEventListener('input', module.update_input)
+			el.addEventListener('input', module.format_input)
 		})
 	})
 	.catch(er => console.error(er))
--- format_number.js
+++ format_number.js
@@ -1,6 +1,13 @@
-function format_string(value, format_string, placeholder = '_') {
+/**
+ * Formats passed string to passed format; returns formatted string and caret position
+ * @param {string} string_to_format 
+ * @param {string} format_string
+ * @param {string} placeholder 
+ * @return {object} {value: formatted_string, pos: caret_position}
+ */
+function format_string(string_to_format, format_string, placeholder = '_') {
 	// Extract numbers 
-	const numbers = value.match(/\d/g)
+	const numbers = string_to_format.match(/\d/g)
 	// If no numbers return empty string and position 0 
 	if (numbers === null) return {
 		value: '',
@@ -31,21 +38,34 @@ function format_string(value, format_string, placeholder = '_') {
 	return response
 }
 
-function format_input(e) {
-	const input = e.target
+/**
+ * Format field's value with placeholder as input mask and return boolean of success
+ * @param {Event} event 
+ * @param {Node} element 
+ * @returns {Boolean} True if was sucsessful false if failed
+ */
+export function format_input(event, element = null) {
+	const exit = err => {
+		console.error(err, input)
+		input.removeEventListener('input', format_input)
+		return false
+	}
+
+	const input = element || event.target
 	const format = input.placeholder
 	const placeholder = input.dataset.placeholder
 
+	// If placeholder attribute not supplied
+	if (!format) return exit('Placeholder attribute is required')
 	// If placeholder character is a number remove the listener
-	if (format.match(/\d+/)) {
-		input.removeEventListener('input', format_input)
-		console.error('Cannot use number as placeholder character on ', input)
-		return
-	}
+	if (format.match(/\d+/)) return exit('Cannot use number as placeholder character')
 
-	const r = format_string(input.value, format, placeholder)
+
+
+	const response = format_string(input.value, format, placeholder)
 	// Set input value to new formatted value
-	input.value = r.value
+	input.value = response.value
 	// Set you cursor to the end of the inputted string
-	input.setSelectionRange(r.pos, r.pos)
+	input.setSelectionRange(response.pos, response.pos)
+	return true
 }
