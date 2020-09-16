86cf632c0cb75ec7177b6f198c719687a5d3b6a8
Daniel Atwood <daniel@atwood.io>
2020-09-16 14:16:08 -0400

    Example file is redundent

--- example.js
+++
@@ -1,8 +0,0 @@
-import('./format_number.js')
-.then(module => {
-		// Listen to all type="tel" inputs
-		document.querySelectorAll('input[type=tel][placeholder]').forEach(el => {
-			el.addEventListener('input', module.format_input)
-		})
-	})
-	.catch(er => console.error(er))
